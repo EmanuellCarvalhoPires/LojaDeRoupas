893e1e0d499be68a6d2eac85da62f436e9f832f8
Emanuell <emanuell.carvalho.pires@gmail.com>
2026-07-06 09:33:06 -0300

    Função para o Menu criada: RegistroVenda() 	Funções auxiliares criadas: 		-VerificarClienteExiste 		-VerificarProdutoExiste 		-ObterValorProduto

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <time.h>
 
 
@@ -29,7 +30,6 @@ typedef struct Reg_Venda {
     char Nome_Produto[50];
     int quantVendida;
     float valorTotal;
-    int ID_produto;
 } Reg_Venda;
 
 void RegistroCliente() {
@@ -107,7 +107,7 @@ void ListarProdutos() {
     printf("-------------------------\n");
 
     // 3. O laço while continua lendo enquanto conseguir capturar os 3 itens da linha
-    // O "%[^,]" significa: "leia tudo até encontrar uma vírgula"
+    // O "%[^, ]" significa: "leia tudo até encontrar uma vírgula"
     while (fscanf(file, "%d,%[^,],%f\n", &id, nome, &valor) == 3) {
 
         // 4. O printf só exibe o que você quer! Ignoramos a variável 'valor' aqui.
@@ -118,6 +118,78 @@ void ListarProdutos() {
 
     fclose(file);
     printf("-------------------------\n");
+}
+
+// Função que verifica se o CPF existe
+int VerificarClienteExiste(char *cpfBuscado) {
+    if (strlen(cpfBuscado) < 11) {
+        printf("Erro: O CPF digitado e invalido. Ele deve conter exatamente 11 numeros!\n");
+        return 0;
+    } // CPF inválido
+
+    FILE *file = fopen(ARQ_CLIENTE, "r");
+    if (file == NULL) return 0; // Arquivo ainda não existe, então o cliente não está lá
+
+    char nomeTemp[50];
+    char cpfTemp[12];
+
+    // O seu RegistroCliente salva no formato: Nome_Cliente.CPF_Cliente
+    while (fscanf(file, "%[^,],%s\n", nomeTemp, cpfTemp) == 2) {
+        // strcmp retorna 0 quando as duas palavras são EXATAMENTE iguais
+        if (strcmp(cpfTemp, cpfBuscado) == 0) {
+            fclose(file);
+            return 1; // Encontrou! Retorna 1 (Verdadeiro)
+        }
+    }
+
+    fclose(file);
+    return 0; // Chegou ao fim do arquivo e não encontrou
+}
+
+// Função que verifica se o Produto existe
+int VerificarProdutoExiste(char *nomeBuscado) {
+    FILE *file = fopen(ARQ_PROD, "r");
+    if (file == NULL) return 0;
+
+    int idTemp;
+    char nomeTemp[50];
+    float valorTemp;
+
+    // O seu RegistroProduto salva no formato: ID_Produto, Nome_Produto, valor_Produto
+    while (fscanf(file, "%d,%[^,],%f\n", &idTemp,nomeTemp,&valorTemp) == 3) {
+        if (strcmp(nomeTemp, nomeBuscado) == 0) {
+            fclose(file);
+            return 1; // Encontrou! Retorna 1 (Verdadeiro)
+        }
+    }
+
+    fclose(file);
+    return 0; // Chegou ao fim do arquivo e não encontrou
+}
+
+// Função que retorna o valor do produto
+float ObterValorProduto(char *nomeBuscado) {
+    FILE *file = fopen(ARQ_PROD, "r");
+    if (file == NULL) {
+        printf("Erro: Arquivo de produtos nao encontrado!\n");
+        return -1.0; // Retorna -1 para indicar que houve um erro
+    }
+
+    int idTemp;
+    char nomeTemp[50];
+    float valorTemp;
+
+    // Varre o arquivo linha por linha
+    while (fscanf(file, "%d,%[^,],%f\n", &idTemp, nomeTemp, &valorTemp) == 3) {
+        // Se encontrar o nome exato
+        if (strcmp(nomeTemp, nomeBuscado) == 0) {
+            fclose(file);
+            return valorTemp; // Encontrou! Retorna o valor do produto imediatamente
+        }
+    }
+
+    fclose(file);
+    return -1.0; // Chegou ao fim do arquivo e nao encontrou o produto
 }
 
 void RegistroVenda() {
@@ -131,12 +203,32 @@ void RegistroVenda() {
 
     printf("Digite o CPF do cliente: ");
     scanf(" %[^\n]", VendaAux.CPF_Cliente);
-
+    int existeSimNao = VerificarClienteExiste(VendaAux.CPF_Cliente);
+    if (existeSimNao != 1) {
+        return;
+    };
 
     printf("Digite o nome do produto do pedido: ");
-    scanf("%s", VendaAux.CPF_Cliente);
+    scanf(" %[^\n]", VendaAux.Nome_Produto);
+    existeSimNao = VerificarProdutoExiste(VendaAux.Nome_Produto);
+    if (existeSimNao != 1) {
+        return;
+    };
 
-    fprintf(file, "%s,%s\n", VendaAux);
+    float valorProduto = ObterValorProduto(VendaAux.Nome_Produto);
+
+    printf("Quantas unidades desse produto foram vendidas?");
+    scanf("%i", &VendaAux.quantVendida);
+    VendaAux.valorTotal = VendaAux.quantVendida * valorProduto;
+
+    time_t t = time(NULL);
+    struct tm tm = *localtime(&t);
+    VendaAux.dataVenda.dia = tm.tm_mday;
+    VendaAux.dataVenda.mes = tm.tm_mon + 1;
+    VendaAux.dataVenda.ano = tm.tm_year + 1900;
+
+    fprintf(file, "%02d/%02d/%d,%s,%s,%i,%f\n",VendaAux.dataVenda.dia,VendaAux.dataVenda.mes,VendaAux.dataVenda.ano,
+        VendaAux.CPF_Cliente, VendaAux.Nome_Produto, VendaAux.quantVendida, VendaAux.valorTotal);
 
     fclose(file);
     printf("Registro criado com sucesso!\n");
@@ -144,7 +236,6 @@ void RegistroVenda() {
 }
 
 int main() {
-
     int aux = 10;
     while (aux != 0) {
         printf("\n========================================\n"
@@ -173,6 +264,7 @@ int main() {
                 ListarProdutos();
                 break;
             case 4:
+                RegistroVenda();
                 break;
         }
     }
